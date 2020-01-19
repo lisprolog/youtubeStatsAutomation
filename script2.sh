c90a4a06c5d5de573824c8b9563f9a1e2ce7be3e
lisprolog <peter.sattarpoor@gmx.at>
2020-01-19 16:51:05 +0100

    curl status + mv folder

--- script2.sh
+++ script2.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 file="/patch/to/videoList2.txt"
+c=1
 while IFS= read -r line
 do
-    echo "$line"
+    echo "$c $line"
+    let "c+=1"
     curl "$line" > youtubefile.txt
     grep "eow-title" youtubefile.txt >> resultFile.txt
     grep "watch-view-count" youtubefile.txt >> resultFile.txt
--- script3.sh
+++ script3.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-file="/patch/to/resultFile.txt"
-
+file="/path/to/resultFile.txt"
+day=$(date +%d)
+month=$(date +%m)
+year=$(date +%y)
 java Extract < "$file" > extractedFile.txt
 java TopTenPriority < extractedFile.txt > TopTen.txt
 rm youtubefile.txt
+mkdir "$year$month$day"
+mv extractedFile.txt resultFile.txt TopTen.txt "$year$month$day"
